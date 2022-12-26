380a7a847e92083eca4052873f4565bd5777618f
gilsh24 <gilsh24@gmail.com>
2022-12-26 16:12:12 +0200

    changes for v2

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Tamar to use React
         </a>
       </header>
     </div>
